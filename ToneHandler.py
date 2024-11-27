4e372d8f5618c2f35faa5ea6d8c380c9ae61cef1
Brayden Sue <bms8@sfu.ca>
2024-11-27 00:36:56 -0800

    removed incorrect code

--- ToneHandler.py
+++ ToneHandler.py
@@ -3,14 +3,8 @@ from AudioHandler import AudioHandler
 
 class ToneHandler:
     def __init__(self, AH: AudioHandler):
-        self.data = self.get_sound_data(AH.get_words())
+        self.data = AH.word_array
         self.sample_width = AH.sample_width
         self.channels = AH.channels
         self.frame_rate = AH.frame_rate
 
-    @staticmethod
-    def get_sound_data(words):
-        raw_data = []
-        for word in words:
-            raw_data.append(word.raw_data)
-        return raw_data
