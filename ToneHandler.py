e01f5ff6b3ca4dca85413220d0f9115ff5ac03ff
braydenmsue <braydenmsue@gmail.com>
2024-12-08 20:01:28 -0800

    fixed 3rd tone

--- ToneHandler.py
+++ ToneHandler.py
@@ -13,7 +13,8 @@ class ToneHandler:
 
     # TODO: try ML model to predict tone (need dataset w/ words and expected tones)
 
-    def get_tone(self, word_f0):
+    def get_tone(self, word_f0, threshold=50):
+
         start, end, global_max, global_min = self.f0_info(word_f0)
         print("------------------------")
         print("START Hz: ", start)
@@ -21,6 +22,31 @@ class ToneHandler:
         print("MAX Hz: ", global_max)
         print("MIN Hz: ", global_min)
         print("------------------------")
+
+        # scaling threshold based on range of f0 values
+        hz_range = abs(global_max - global_min)
+        if hz_range > threshold * 1.5:  # arbitrary scaling factor
+            print("OLD THRESHOLD:", threshold)
+            threshold = hz_range * 0.5  # arbitrary scaling factor
+            print("NEW THRESHOLD:", threshold)
+
+        # if starts and ends in same place; range is small (tone is consistent)
+        if abs(end - start) < threshold and abs(global_max - global_min) < threshold:
+            print("Tone: 1")
+        # if max is near the end and min is near the start
+        elif abs(global_max - end) < threshold and abs(global_min - start) < threshold:
+            print("Tone: 2")
+        # start and end are close, but range is large
+        elif abs(end - start) < threshold < hz_range:
+            print("Tone: 3")
+        elif start > global_min and end > global_min:
+            print("Tone: 3 (LESS CERTAIN)")
+        elif abs(global_max - start) < threshold and abs(global_min - end) < threshold:
+            print("Tone: 4")
+        else:
+            print("DON'T KNOW")
+
+        print("\n")
 
 
     def f0_info(self, f0_array):
@@ -40,5 +66,5 @@ class ToneHandler:
 
     def reduce_noise(self, word):
         word_filtered = nr.reduce_noise(y=word, sr=self.frame_rate)
-        word_trimmed = librosa.effects.trim(word_filtered, top_db=8)[0]
+        word_trimmed = librosa.effects.trim(word_filtered, top_db=10)[0]
         return word_trimmed
